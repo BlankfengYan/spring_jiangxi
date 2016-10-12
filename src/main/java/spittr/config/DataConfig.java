77b95938eb2d2b986c5536e6d5a08789dfcd45fe
cmatcing <yaojie.yan@gmail.com>
2016-10-12 23:45:04 +0800

    add gradle build

--- src/main/java/spittr/config/DataConfig.java
+++ src/main/java/spittr/config/DataConfig.java
@@ -6,28 +6,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-//import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class DataConfig {
 
   @Bean
   public DataSource dataSource() {
+      /*
     System.out.println("Init mysql database");
     DriverManagerDataSource ds = new DriverManagerDataSource();
     ds.setDriverClassName("org.h2.Driver");
-    ds.setUrl("jdbc:h2:tcp://localhost/~/spitter");
+    ds.setUrl("jdbc:h2:tcp://localhost:3306/~/spitter");
     ds.setUsername("root");
     ds.setPassword("abc123");
     return ds;
-    /*
+    */
     return new EmbeddedDatabaseBuilder()
             .setType(EmbeddedDatabaseType.H2)
             .addScript("schema.sql")
             .build();
-    */
   }
   
   @Bean
